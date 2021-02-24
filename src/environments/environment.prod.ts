67fb30427fcced63c3aad37b4f1261ee27e7aeb7
Luke <laleggio@ucdavis.edu>
2021-02-23 18:15:41 -0800

    Prod api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: true,
-  apiUrl:'http://magicweb-env.eba-m3p8ff6u.us-west-2.elasticbeanstalk.com',
-  webSocketUrl:"ws://magicweb-env.eba-m3p8ff6u.us-west-2.elasticbeanstalk.com"
+  apiUrl:'https://api.mtgontheweb.com',
+  webSocketUrl:"ws://api.mtgontheweb.com"
 };
